bf16c97116aba98862733ecea36701ca2e529c50
dmitry <dmitry@192.168.147.32>
2018-02-09 11:04:53 +0200

    bet fix

--- src/main/java/com/athena/backend/platform/dto/game/PlaceBetRequest.java
+++ src/main/java/com/athena/backend/platform/dto/game/PlaceBetRequest.java
@@ -10,22 +10,24 @@ public final class PlaceBetRequest {
 	public BetType type;
 	public String teamId;
 	public String playerId;
-	public Integer x;
-	public Integer y;
+	public GameLocation location;
 	public Long clientTime;
+	public String teamName;
+	public String playerName;
 	@Override
 	public final int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((clientTime == null) ? 0 : clientTime.hashCode());
 		result = prime * result + ((gameId == null) ? 0 : gameId.hashCode());
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
 		result = prime * result + ((playerId == null) ? 0 : playerId.hashCode());
+		result = prime * result + ((playerName == null) ? 0 : playerName.hashCode());
 		result = prime * result + stakeAmount;
 		result = prime * result + ((teamId == null) ? 0 : teamId.hashCode());
+		result = prime * result + ((teamName == null) ? 0 : teamName.hashCode());
 		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		result = prime * result + winAmount;
-		result = prime * result + ((x == null) ? 0 : x.hashCode());
-		result = prime * result + ((y == null) ? 0 : y.hashCode());
 		return result;
 	}
 	@Override
@@ -47,10 +49,17 @@ public final class PlaceBetRequest {
 				return false;
 		} else if (!gameId.equals(other.gameId))
 			return false;
+		if (location != other.location)
+			return false;
 		if (playerId == null) {
 			if (other.playerId != null)
 				return false;
 		} else if (!playerId.equals(other.playerId))
+			return false;
+		if (playerName == null) {
+			if (other.playerName != null)
+				return false;
+		} else if (!playerName.equals(other.playerName))
 			return false;
 		if (stakeAmount != other.stakeAmount)
 			return false;
@@ -59,26 +68,21 @@ public final class PlaceBetRequest {
 				return false;
 		} else if (!teamId.equals(other.teamId))
 			return false;
+		if (teamName == null) {
+			if (other.teamName != null)
+				return false;
+		} else if (!teamName.equals(other.teamName))
+			return false;
 		if (type != other.type)
 			return false;
 		if (winAmount != other.winAmount)
-			return false;
-		if (x == null) {
-			if (other.x != null)
-				return false;
-		} else if (!x.equals(other.x))
-			return false;
-		if (y == null) {
-			if (other.y != null)
-				return false;
-		} else if (!y.equals(other.y))
 			return false;
 		return true;
 	}
 	@Override
 	public final String toString() {
 		return "PlaceBetRequest [gameId=" + gameId + ", stakeAmount=" + stakeAmount + ", winAmount=" + winAmount
-				+ ", type=" + type + ", teamId=" + teamId + ", playerId=" + playerId + ", x=" + x + ", y=" + y
-				+ ", clientTime=" + clientTime + "]";
+				+ ", type=" + type + ", teamId=" + teamId + ", playerId=" + playerId + ", location=" + location
+				+ ", clientTime=" + clientTime + ", teamName=" + teamName + ", playerName=" + playerName + "]";
 	}
 }
