44b93c68e9a47eb1e3dddc46a8bf3000c032839b
dmitry <dmitry@192.168.147.32>
2018-02-12 20:13:57 +0200

    qrt

--- src/main/java/com/athena/backend/platform/dto/game/PlaceBetRequest.java
+++ src/main/java/com/athena/backend/platform/dto/game/PlaceBetRequest.java
@@ -24,8 +24,10 @@ public final class PlaceBetRequest {
 	public String teamName;
 	
 	@JsonProperty(required=true)
-	public Long clientTime;
+	public long clientTime;
 	
+	@JsonProperty(required=true)
+	public int quarter;
 	
 	
 	@JsonProperty(required=false)
@@ -36,16 +38,16 @@ public final class PlaceBetRequest {
 	
 	@JsonProperty(required=false)
 	public String playerName;
-	
+
 	@Override
 	public final int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((clientTime == null) ? 0 : clientTime.hashCode());
 		result = prime * result + ((gameId == null) ? 0 : gameId.hashCode());
 		result = prime * result + ((location == null) ? 0 : location.hashCode());
 		result = prime * result + ((playerId == null) ? 0 : playerId.hashCode());
 		result = prime * result + ((playerName == null) ? 0 : playerName.hashCode());
+		result = prime * result + quarter;
 		result = prime * result + stakeAmount;
 		result = prime * result + ((teamId == null) ? 0 : teamId.hashCode());
 		result = prime * result + ((teamName == null) ? 0 : teamName.hashCode());
@@ -62,11 +64,6 @@ public final class PlaceBetRequest {
 		if (getClass() != obj.getClass())
 			return false;
 		final PlaceBetRequest other = (PlaceBetRequest) obj;
-		if (clientTime == null) {
-			if (other.clientTime != null)
-				return false;
-		} else if (!clientTime.equals(other.clientTime))
-			return false;
 		if (gameId == null) {
 			if (other.gameId != null)
 				return false;
@@ -83,6 +80,8 @@ public final class PlaceBetRequest {
 			if (other.playerName != null)
 				return false;
 		} else if (!playerName.equals(other.playerName))
+			return false;
+		if (quarter != other.quarter)
 			return false;
 		if (stakeAmount != other.stakeAmount)
 			return false;
@@ -105,7 +104,8 @@ public final class PlaceBetRequest {
 	@Override
 	public final String toString() {
 		return "PlaceBetRequest [gameId=" + gameId + ", stakeAmount=" + stakeAmount + ", winAmount=" + winAmount
-				+ ", type=" + type + ", teamId=" + teamId + ", playerId=" + playerId + ", location=" + location
-				+ ", clientTime=" + clientTime + ", teamName=" + teamName + ", playerName=" + playerName + "]";
+				+ ", type=" + type + ", teamId=" + teamId + ", teamName=" + teamName + ", clientTime=" + clientTime
+				+ ", quarter=" + quarter + ", playerId=" + playerId + ", location=" + location + ", playerName="
+				+ playerName + "]";
 	}
 }
