265757aa956f5c40b7c93534e47e17ec76f19aa0
dmitry <dmitry@192.168.147.32>
2018-02-16 13:12:25 +0200

    mmm

---
+++ src/main/java/com/athena/backend/platform/dto/events/RabbitEvent.java
@@ -0,0 +1,91 @@
+package com.athena.backend.platform.dto.events;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class RabbitEvent {
+	@JsonProperty(required=false)
+	public GeneralEvent g;
+	
+	@JsonProperty(required=false)
+	public GeneralEvent m;
+	
+	@JsonProperty(required=false)
+	public TickEvent t;
+	
+	@JsonProperty(required=true)
+	public Type tp;
+	
+	public RabbitEvent() {}
+	
+	public static final RabbitEvent meta(final GeneralEvent event) {
+		final RabbitEvent res = new RabbitEvent();
+		res.tp = Type.m;
+		res.m = event;
+		return res;
+	}
+	
+	public static final RabbitEvent tick(final TickEvent event) {
+		final RabbitEvent res = new RabbitEvent();
+		res.tp = Type.t;
+		res.t = event;
+		return res;
+	}
+	
+	public static final RabbitEvent general(final GeneralEvent event) {
+		final RabbitEvent res = new RabbitEvent();
+		res.tp = Type.g;
+		res.g = event;
+		return res;
+	}
+	
+	public static enum Type{
+		g,m,t
+	}
+
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((g == null) ? 0 : g.hashCode());
+		result = prime * result + ((m == null) ? 0 : m.hashCode());
+		result = prime * result + ((t == null) ? 0 : t.hashCode());
+		result = prime * result + ((tp == null) ? 0 : tp.hashCode());
+		return result;
+	}
+
+	@Override
+	public final boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final RabbitEvent other = (RabbitEvent) obj;
+		if (g == null) {
+			if (other.g != null)
+				return false;
+		} else if (!g.equals(other.g))
+			return false;
+		if (m == null) {
+			if (other.m != null)
+				return false;
+		} else if (!m.equals(other.m))
+			return false;
+		if (t == null) {
+			if (other.t != null)
+				return false;
+		} else if (!t.equals(other.t))
+			return false;
+		if (tp != other.tp)
+			return false;
+		return true;
+	}
+
+	@Override
+	public final String toString() {
+		return "RabbitEvent [g=" + g + ", m=" + m + ", t=" + t + ", tp=" + tp + "]";
+	}
+}
