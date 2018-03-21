761d64a1cd2f02c23d18452233ff971509749d8d
dmitry <dmitry@192.168.147.55>
2018-03-21 18:40:17 +0200

    mmm

--- src/main/java/com/athena/backend/platform/dto/users/UserXpDTO.java
+++ src/main/java/com/athena/backend/platform/dto/users/UserXpDTO.java
@@ -2,13 +2,16 @@ package com.athena.backend.platform.dto.users;
 
 public final class UserXpDTO {
 	public final int xp;
-	public UserXpDTO(final int xp) {
+	public final String title;
+	public UserXpDTO(final int xp, final String title) {
 		this.xp = xp;
+		this.title = title;
 	}
 	@Override
 	public final int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		result = prime * result + xp;
 		return result;
 	}
@@ -21,12 +24,17 @@ public final class UserXpDTO {
 		if (getClass() != obj.getClass())
 			return false;
 		final UserXpDTO other = (UserXpDTO) obj;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
 		if (xp != other.xp)
 			return false;
 		return true;
 	}
 	@Override
 	public final String toString() {
-		return "UserXpDTO [xp=" + xp + "]";
+		return "UserXpDTO [xp=" + xp + ", title=" + title + "]";
 	}
 }
