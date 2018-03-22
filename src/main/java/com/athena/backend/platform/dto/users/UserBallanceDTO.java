a468439202990993bbc392a6801c0bdc1256230c
Dmitry Paradny <dmitry@Dmitrys-MacBook-Pro.local>
2018-03-22 12:31:14 +0200

    fff

--- src/main/java/com/athena/backend/platform/dto/users/UserBallanceDTO.java
+++ src/main/java/com/athena/backend/platform/dto/users/UserBallanceDTO.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public final class UserBallanceDTO {
 	public double rank;
 	public int ballance;
+	public UserXpDTO experience;
 	@Override
 	public final int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ballance;
+		result = prime * result + ((experience == null) ? 0 : experience.hashCode());
 		long temp;
 		temp = Double.doubleToLongBits(rank);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
@@ -27,12 +29,17 @@ public final class UserBallanceDTO {
 		final UserBallanceDTO other = (UserBallanceDTO) obj;
 		if (ballance != other.ballance)
 			return false;
+		if (experience == null) {
+			if (other.experience != null)
+				return false;
+		} else if (!experience.equals(other.experience))
+			return false;
 		if (Double.doubleToLongBits(rank) != Double.doubleToLongBits(other.rank))
 			return false;
 		return true;
 	}
 	@Override
 	public final String toString() {
-		return "UserBallanceDTO [rank=" + rank + ", ballance=" + ballance + "]";
+		return "UserBallanceDTO [rank=" + rank + ", ballance=" + ballance + ", experience=" + experience + "]";
 	}
 }
