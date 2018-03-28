513292bb31fa603311ef640fa73177bb146d725d
dmitry <dmitry@192.168.147.37>
2018-03-28 11:54:00 +0300

    time

--- src/main/java/com/athena/backend/platform/dto/sportradar/nba/Game.java
+++ src/main/java/com/athena/backend/platform/dto/sportradar/nba/Game.java
@@ -18,21 +18,13 @@ public final class Game {
 	public int quarter;
 	public String clock;
 	public boolean clockRunning;
+	public long timeLeftBeforeTheGame;
+	
 	@Override
 	public final int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((away == null) ? 0 : away.hashCode());
-		result = prime * result + away_points;
-		result = prime * result + ((clock == null) ? 0 : clock.hashCode());
-		result = prime * result + (clockRunning ? 1231 : 1237);
-		result = prime * result + ((coverage == null) ? 0 : coverage.hashCode());
-		result = prime * result + ((home == null) ? 0 : home.hashCode());
-		result = prime * result + home_points;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + quarter;
-		result = prime * result + ((scheduled == null) ? 0 : scheduled.hashCode());
-		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		return result;
 	}
 	@Override
@@ -44,48 +36,10 @@ public final class Game {
 		if (getClass() != obj.getClass())
 			return false;
 		final Game other = (Game) obj;
-		if (away == null) {
-			if (other.away != null)
-				return false;
-		} else if (!away.equals(other.away))
-			return false;
-		if (away_points != other.away_points)
-			return false;
-		if (clock == null) {
-			if (other.clock != null)
-				return false;
-		} else if (!clock.equals(other.clock))
-			return false;
-		if (clockRunning != other.clockRunning)
-			return false;
-		if (coverage == null) {
-			if (other.coverage != null)
-				return false;
-		} else if (!coverage.equals(other.coverage))
-			return false;
-		if (home == null) {
-			if (other.home != null)
-				return false;
-		} else if (!home.equals(other.home))
-			return false;
-		if (home_points != other.home_points)
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (quarter != other.quarter)
-			return false;
-		if (scheduled == null) {
-			if (other.scheduled != null)
-				return false;
-		} else if (!scheduled.equals(other.scheduled))
-			return false;
-		if (status == null) {
-			if (other.status != null)
-				return false;
-		} else if (!status.equals(other.status))
 			return false;
 		return true;
 	}
@@ -93,6 +47,7 @@ public final class Game {
 	public final String toString() {
 		return "Game [id=" + id + ", status=" + status + ", coverage=" + coverage + ", scheduled=" + scheduled
 				+ ", home_points=" + home_points + ", away_points=" + away_points + ", home=" + home + ", away=" + away
-				+ ", quarter=" + quarter + ", clock=" + clock + ", clockRunning=" + clockRunning + "]";
+				+ ", quarter=" + quarter + ", clock=" + clock + ", clockRunning=" + clockRunning
+				+ ", timeLeftBeforeTheGame=" + timeLeftBeforeTheGame + "]";
 	}
 }
