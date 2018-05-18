3ee47dd91e2d89ad72cffdf130203a50e04a32e8
dmitry <dmitry@192.168.147.185>
2018-05-18 16:49:41 +0300

    invite to group dto

---
+++ src/main/java/com/athena/backend/platform/dto/users/groups/InviteToGroupLinkDTO.java
@@ -0,0 +1,41 @@
+package com.athena.backend.platform.dto.users.groups;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class InviteToGroupLinkDTO {
+	@JsonProperty(required=true)
+	public final String url;
+	
+	public InviteToGroupLinkDTO(final String url) {
+		this.url = url;
+	}
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+	@Override
+	public final boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final InviteToGroupLinkDTO other = (InviteToGroupLinkDTO) obj;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+	@Override
+	public final String toString() {
+		return "InviteToGroupLinkDTO [url=" + url + "]";
+	}
+}
