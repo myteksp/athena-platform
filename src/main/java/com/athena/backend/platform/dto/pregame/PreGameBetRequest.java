6e4f0e410363f11b0171c56c34b61e4e4da969fd
dmitry <dmitry@192.168.193.150>
2018-07-24 10:48:25 +0300

    mmmm

--- src/main/java/com/athena/backend/platform/dto/pregame/PreGameBetRequest.java
+++ src/main/java/com/athena/backend/platform/dto/pregame/PreGameBetRequest.java
@@ -25,5 +25,70 @@ public final class PreGameBetRequest {
 	public double winAmount;
 	@JsonProperty(required=false)
 	public double winningThreshold;
-	
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((gameId == null) ? 0 : gameId.hashCode());
+		result = prime * result + ((nonOfThePlayers == null) ? 0 : nonOfThePlayers.hashCode());
+		result = prime * result + ((playerId == null) ? 0 : playerId.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(riskAmount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((scoreBet == null) ? 0 : scoreBet.hashCode());
+		result = prime * result + ((teamId == null) ? 0 : teamId.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		temp = Double.doubleToLongBits(winAmount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(winningThreshold);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public final boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final PreGameBetRequest other = (PreGameBetRequest) obj;
+		if (gameId == null) {
+			if (other.gameId != null)
+				return false;
+		} else if (!gameId.equals(other.gameId))
+			return false;
+		if (nonOfThePlayers == null) {
+			if (other.nonOfThePlayers != null)
+				return false;
+		} else if (!nonOfThePlayers.equals(other.nonOfThePlayers))
+			return false;
+		if (playerId == null) {
+			if (other.playerId != null)
+				return false;
+		} else if (!playerId.equals(other.playerId))
+			return false;
+		if (Double.doubleToLongBits(riskAmount) != Double.doubleToLongBits(other.riskAmount))
+			return false;
+		if (scoreBet != other.scoreBet)
+			return false;
+		if (teamId == null) {
+			if (other.teamId != null)
+				return false;
+		} else if (!teamId.equals(other.teamId))
+			return false;
+		if (type != other.type)
+			return false;
+		if (Double.doubleToLongBits(winAmount) != Double.doubleToLongBits(other.winAmount))
+			return false;
+		if (Double.doubleToLongBits(winningThreshold) != Double.doubleToLongBits(other.winningThreshold))
+			return false;
+		return true;
+	}
+	@Override
+	public final String toString() {
+		return "PreGameBetRequest [type=" + type + ", gameId=" + gameId + ", teamId=" + teamId + ", playerId="
+				+ playerId + ", nonOfThePlayers=" + nonOfThePlayers + ", scoreBet=" + scoreBet + ", riskAmount="
+				+ riskAmount + ", winAmount=" + winAmount + ", winningThreshold=" + winningThreshold + "]";
+	}
 }
