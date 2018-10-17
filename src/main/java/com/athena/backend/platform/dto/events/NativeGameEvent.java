4a449cdb2321f2d2c1f408b4348a2b31d4775e42
dmitry <dmitry@192.168.193.234>
2018-10-17 16:52:07 +0300

    mmmmmmm

--- src/main/java/com/athena/backend/platform/dto/events/NativeGameEvent.java
+++ src/main/java/com/athena/backend/platform/dto/events/NativeGameEvent.java
@@ -577,6 +577,33 @@ public final class NativeGameEvent extends BaseNativeEvent{
 		public int year;
 		public String type;
 		public List<Team> teams;
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final Season other = (Season) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
+		}
+		@Override
+		public final String toString() {
+			return "Season [id=" + id + ", year=" + year + ", type=" + type + ", teams=" + teams + "]";
+		}
 	}
 	
 	@JsonIgnoreProperties(ignoreUnknown = true)
