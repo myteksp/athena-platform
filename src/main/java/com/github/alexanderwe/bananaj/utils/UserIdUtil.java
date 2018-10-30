9a69ff50131b262197d0de4f6d77e0a913ae1ffc
dmitry <dmitry@192.168.196.69>
2018-10-30 11:29:36 +0200

    util

---
+++ src/main/java/com/github/alexanderwe/bananaj/utils/UserIdUtil.java
@@ -0,0 +1,21 @@
+package com.github.alexanderwe.bananaj.utils;
+
+import com.gf.util.string.MacroCompiler;
+
+public final class UserIdUtil {
+	public static final String wrap(final String id) {
+		if (id == null)
+			return null;
+		return MacroCompiler.compileInline("[[${0}]]", id.trim());
+	}
+
+	public static final String unwrap(final String id) {
+		if (id == null)
+			return null;
+		final String trimed = id.trim();
+		if (trimed.startsWith("[[") && trimed.endsWith("]]")) 
+			return trimed.substring(2, trimed.length() - 2);
+		else 
+			return trimed;
+	}
+}
