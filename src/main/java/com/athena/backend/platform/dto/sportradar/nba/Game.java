b16df3124aa3925a013d7cac1870aa9b704d195a
dmitry <dmitry@192.168.194.115>
2018-11-21 12:26:06 +0200

    mmmmmmm

--- src/main/java/com/athena/backend/platform/dto/sportradar/nba/Game.java
+++ src/main/java/com/athena/backend/platform/dto/sportradar/nba/Game.java
@@ -22,7 +22,7 @@ public final class Game {
 	public String leagueName;
 	public String leagueAlias;
 	public String periodType;
-	public String network;
+	public Broadcast broadcast;
 	
 	@Override
 	public final int hashCode() {
@@ -53,6 +53,46 @@ public final class Game {
 				+ ", home_points=" + home_points + ", away_points=" + away_points + ", home=" + home + ", away=" + away
 				+ ", quarter=" + quarter + ", clock=" + clock + ", clockRunning=" + clockRunning
 				+ ", timeLeftBeforeTheGame=" + timeLeftBeforeTheGame + ", leagueName=" + leagueName + ", leagueAlias="
-				+ leagueAlias + ", periodType=" + periodType + ", network=" + network + "]";
+				+ leagueAlias + ", periodType=" + periodType + ", broadcast=" + broadcast + "]";
+	}
+
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static final class Broadcast{
+		public String network;
+		public String satellite;
+		@Override
+		public final int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((network == null) ? 0 : network.hashCode());
+			result = prime * result + ((satellite == null) ? 0 : satellite.hashCode());
+			return result;
+		}
+		@Override
+		public final boolean equals(final Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final Broadcast other = (Broadcast) obj;
+			if (network == null) {
+				if (other.network != null)
+					return false;
+			} else if (!network.equals(other.network))
+				return false;
+			if (satellite == null) {
+				if (other.satellite != null)
+					return false;
+			} else if (!satellite.equals(other.satellite))
+				return false;
+			return true;
+		}
+		@Override
+		public final String toString() {
+			return "Broadcast [network=" + network + ", satellite=" + satellite + "]";
+		}
 	}
 }
