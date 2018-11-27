0146c1315dc823034a0ef982f61a1cfb6cb49609
dmitry <dmitry@192.168.195.21>
2018-11-27 14:06:46 +0200

    mmmmm

---
+++ src/main/java/com/athena/backend/platform/dto/users/PushNotificationMessageDTO.java
@@ -0,0 +1,42 @@
+package com.athena.backend.platform.dto.users;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class PushNotificationMessageDTO {
+	public String title;
+	public String body;
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((body == null) ? 0 : body.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	@Override
+	public final boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final PushNotificationMessageDTO other = (PushNotificationMessageDTO) obj;
+		if (body == null) {
+			if (other.body != null)
+				return false;
+		} else if (!body.equals(other.body))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	@Override
+	public final String toString() {
+		return "PushNotificationMessageDTO [title=" + title + ", body=" + body + "]";
+	}
+}
