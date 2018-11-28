611642744b5a7dfc704b54f4ddc0bffaddf86d39
dmitry <dmitry@192.168.195.21>
2018-11-28 19:31:25 +0200

    mmmmmmmmm

---
+++ src/main/java/com/athena/backend/platform/dto/achievments/Badge.java
@@ -0,0 +1,59 @@
+package com.athena.backend.platform.dto.achievments;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(value=Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class Badge {
+	public String id;
+	public boolean active;
+	public boolean visible;
+	public String name;
+	public String description;
+	public String popUpText;
+	public Map<String, Object> custom_data;
+	public List<Currency> rewards;
+	public String segmentJS;
+	public String conditionJS;
+	
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public final boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Badge other = (Badge) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public final String toString() {
+		return "Badge [id=" + id + ", active=" + active + ", visible=" + visible + ", name=" + name + ", description="
+				+ description + ", popUpText=" + popUpText + ", custom_data=" + custom_data + ", rewards=" + rewards
+				+ ", segmentJS=" + segmentJS + ", conditionJS=" + conditionJS + "]";
+	}
+}
