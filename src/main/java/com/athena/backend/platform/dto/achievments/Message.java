72a7b5004cc83af1ad59f0a1b84160cf190c26d5
dmitry <dmitry@192.168.199.2>
2019-01-15 19:44:37 +0200

    message dto completed

--- src/main/java/com/athena/backend/platform/dto/achievments/Message.java
+++ src/main/java/com/athena/backend/platform/dto/achievments/Message.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gf.util.string.JSON;
 
 @JsonInclude(value= JsonInclude.Include.NON_NULL)
@@ -16,18 +17,44 @@ public final class Message {
 	private static final String charset = "UTF-8";
 	private static final Encoder encoder = Base64.getUrlEncoder();
 	private static final Decoder decoder = Base64.getUrlDecoder();
-
+	
+	@JsonProperty(required=true)
 	public String id;
+	
+	@JsonProperty(required=true)
 	public String userId;
+	
+	@JsonProperty(required=true)
 	public Type type;
+	
+	@JsonProperty(required=false)
 	public Status status;
+	
+	@JsonProperty(required=false)
 	public String title;
+	
+	@JsonProperty(required=false)
 	public String body;
+	
+	@JsonProperty(required=false)
 	public Date sent;
+	
+	@JsonProperty(required=false)
 	public Date opened;
+	
+	@JsonProperty(required=true)
+	public long timeToLive;
+	
+	@JsonProperty(required=false)
 	public String postBackUrl;
+	
+	@JsonProperty(required=false)
 	public String deepLink;
+	
+	@JsonProperty(required=false)
 	public List<String> errors;
+	
+	@JsonProperty(required=true)
 	public volatile boolean encoded = false;
 
 	public final Message encodeTitleAndBody() {
