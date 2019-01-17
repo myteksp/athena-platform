64ed69593e412b5936ed0e8c775c40e954061c93
dmitry <dmitry@192.168.194.220>
2019-01-17 21:14:25 +0200

    fields

--- src/main/java/com/athena/backend/platform/dto/achievments/MessagingCampaign.java
+++ src/main/java/com/athena/backend/platform/dto/achievments/MessagingCampaign.java
@@ -8,72 +8,78 @@ import com.gf.util.string.JSON;
 @JsonInclude(value= JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public final class MessagingCampaign {
-	
+
 	@JsonProperty(required=false)
 	public String id;
-	
+
 	@JsonProperty(required=false)
 	public String application;
-	
+
 	@JsonProperty(required=true)
 	public String name;
-	
+
 	@JsonProperty(required=true)
 	public Type type;
-	
+
 	@JsonProperty(required=true)
 	public Status status;
-	
+
 	@JsonProperty(required=true)
 	public Message.Type messageType;
-	
+
 	@JsonProperty(required=true)
 	public Message.BodyType messageBodyType;
-	
+
 	@JsonProperty(required=true)
 	public String messageTitle;
-	
+
 	@JsonProperty(required=true)
 	public String messageBody;
-	
+
 	@JsonProperty(required=true)
 	public long messageTimeToLive;
-	
+
 	@JsonProperty(required=false)
 	public String segmentationScript;
-	
+
 	@JsonProperty(required=false)
 	public String reactiveScript;
-	
+
 	@JsonProperty(required=false)
 	public String userId;
-	
-	
+
+
 	@JsonProperty(required=false)
 	public Long sent;
-	
+
 	@JsonProperty(required=false)
 	public Long pending;
-	
+
 	@JsonProperty(required=false)
 	public Long opened;
-	
+
 	@JsonProperty(required=false)
 	public Long failed;
-	
+
 	@JsonProperty(required=false)
 	public Long scheduledCount;
-	
+
 	@JsonProperty(required=false)
 	public Long created;
-	
+
 	@JsonProperty(required=false)
 	public Long edited;
-	
+
+	@JsonProperty(required=false)
+	public Boolean inProcess;
+
+	@JsonProperty(required=false)
+	public Long targetAudience;
+
 	public static enum Type{
 		REACTIVE, INSTANT, USER
 	}
-	
+
 	public static enum Status{
 		ACTIVE, INACTIVE
 	}
