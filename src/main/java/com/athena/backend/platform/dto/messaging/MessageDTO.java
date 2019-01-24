fead0f3ffbbb92572432ba2a5651a720d9cda420
dmitry <dmitry@dmitrys-macbook-pro.local>
2019-01-24 16:16:44 +0200

    mmmmmm

---
+++ src/main/java/com/athena/backend/platform/dto/messaging/MessageDTO.java
@@ -0,0 +1,50 @@
+package com.athena.backend.platform.dto.messaging;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(value=Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class MessageDTO {
+	@JsonProperty(required=true)
+	public String id;
+	
+	@JsonProperty(required=true)
+	public Object data;
+	
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public final boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final MessageDTO other = (MessageDTO) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public final String toString() {
+		return "MessageDTO [id=" + id + ", data=" + data + "]";
+	}
+}
