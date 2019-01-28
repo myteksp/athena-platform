236208852b268b0599e479de8cd1cda62a7a49c9
dmitry <dmitry@192.168.10.98>
2019-01-28 13:32:02 +0200

    mmmmmmmm

---
+++ src/main/java/com/github/alexanderwe/bananaj/utils/ParamsParser.java
@@ -0,0 +1,34 @@
+package com.github.alexanderwe.bananaj.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.gf.collections.GfCollection;
+import com.gf.collections.GfCollections;
+import com.gf.collections.tuples.Tuples;
+import com.gf.util.string.Splitter;
+
+public final class ParamsParser {
+	public static final Map<String, GfCollection<String>> parseQueryParams(final String uri){
+		final Map<String, GfCollection<String>> res = new HashMap<String, GfCollection<String>>();
+		GfCollections.asArrayCollection(Splitter.split(uri, '&'))
+		.flatMap(pair->{
+			final GfCollection<String> p = GfCollections.asArrayCollection(Splitter.split(pair, '='));
+			if (p.size() < 2)
+				return GfCollections.asLinkedCollection();
+			if (p.size() == 2) 
+				return GfCollections.asLinkedCollection(Tuples.get(p.findFirst().trim(), p.get(1)));
+			return GfCollections.asLinkedCollection(Tuples.get(p.findFirst().trim(), p.takeFromEnd(p.size() - 1).join("=")));
+		})
+		.forEach(t->{
+			GfCollection<String> col = res.get(t.v1);
+			if (col == null) {
+				col = GfCollections.asLinkedCollection();
+				res.put(t.v1, col);
+			}
+			col.add(t.v2);
+		});
+		return res;
+	}
+	
+}
