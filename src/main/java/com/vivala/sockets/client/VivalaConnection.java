48dc3957965cc449622a51e5590ba1652c685a4d
dmitry <dmitry@192.168.0.104>
2019-02-05 23:49:52 +0200

    http client

--- src/main/java/com/vivala/sockets/client/VivalaConnection.java
+++ src/main/java/com/vivala/sockets/client/VivalaConnection.java
@@ -3,5 +3,4 @@ package com.vivala.sockets.client;
 public interface VivalaConnection extends VivalaSender{
 	void addHandler(final MessageHandler handler);
 	void deleteHandler(final MessageHandler handler);
-	void close();
 }
--- src/main/java/com/vivala/sockets/client/VivalaSender.java
+++ src/main/java/com/vivala/sockets/client/VivalaSender.java
@@ -3,4 +3,5 @@ package com.vivala.sockets.client;
 public interface VivalaSender {
 	void send(final String message);
 	void sendNotSafe(final String message);
+	void close();
 }
