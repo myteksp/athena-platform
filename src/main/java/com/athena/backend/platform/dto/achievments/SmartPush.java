0ffcab9e6c613496a7301dcc66cc92b7e3d66bb1
dmitry <dmitry@192.168.193.23>
2019-02-13 19:13:35 +0200

    smart push

--- src/main/java/com/athena/backend/platform/dto/achievments/SmartPush.java
+++ src/main/java/com/athena/backend/platform/dto/achievments/SmartPush.java
@@ -1,10 +1,12 @@
 package com.athena.backend.platform.dto.achievments;
 
 import com.athena.backend.platform.dto.events.NativeGameEvent.GameParticipantPlayer;
+import com.athena.backend.platform.dto.pregame.PreGameBetData;
 import com.athena.backend.platform.dto.sportradar.nba.Game;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.gf.util.string.JSON;
 
 @JsonInclude(value= JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -28,6 +30,9 @@ public final class SmartPush {
 	public Game game;
 	
 	@JsonProperty(required=false)
+	public PreGameBetData pregameData;
+	
+	@JsonProperty(required=false)
 	public GameParticipantPlayer player; 
 	
 	public static enum Type{
@@ -36,16 +41,7 @@ public final class SmartPush {
 
 	@Override
 	public final int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((game == null) ? 0 : game.hashCode());
-		result = prime * result + ((gameId == null) ? 0 : gameId.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((player == null) ? 0 : player.hashCode());
-		result = prime * result + ((playerId == null) ? 0 : playerId.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-		return result;
+		return this.toString().hashCode();
 	}
 	@Override
 	public final boolean equals(final Object obj) {
@@ -56,43 +52,10 @@ public final class SmartPush {
 		if (getClass() != obj.getClass())
 			return false;
 		final SmartPush other = (SmartPush) obj;
-		if (game == null) {
-			if (other.game != null)
-				return false;
-		} else if (!game.equals(other.game))
-			return false;
-		if (gameId == null) {
-			if (other.gameId != null)
-				return false;
-		} else if (!gameId.equals(other.gameId))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (player == null) {
-			if (other.player != null)
-				return false;
-		} else if (!player.equals(other.player))
-			return false;
-		if (playerId == null) {
-			if (other.playerId != null)
-				return false;
-		} else if (!playerId.equals(other.playerId))
-			return false;
-		if (type != other.type)
-			return false;
-		if (userId == null) {
-			if (other.userId != null)
-				return false;
-		} else if (!userId.equals(other.userId))
-			return false;
-		return true;
+		return this.toString().equals(other.toString());
 	}
 	@Override
 	public final String toString() {
-		return "SmartPush [id=" + id + ", type=" + type + ", userId=" + userId + ", gameId=" + gameId + ", playerId="
-				+ playerId + ", game=" + game + ", player=" + player + "]";
+		return JSON.toJson(this);
 	}
 }
