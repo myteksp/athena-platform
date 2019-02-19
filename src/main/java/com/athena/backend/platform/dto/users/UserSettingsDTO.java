1f57efec095380adccf5808416df5c7e1ff3a166
dmitry <dmitry@192.168.194.80>
2019-02-19 17:24:55 +0200

    mmmmmmm

---
+++ src/main/java/com/athena/backend/platform/dto/users/UserSettingsDTO.java
@@ -0,0 +1,32 @@
+package com.athena.backend.platform.dto.users;
+
+import com.gf.util.string.JSON;
+
+public final class UserSettingsDTO {
+	public HomeSceenWidget homeWidget;
+	public String homeWidgetParam;
+	
+	public static enum HomeSceenWidget{
+		MINIGAME, APP_PAGE, SHOP_ITEM, CHALLANGE
+	}
+
+	@Override
+	public final int hashCode() {
+		return this.toString().hashCode();
+	}
+	@Override
+	public final boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final UserSettingsDTO other = (UserSettingsDTO) obj;
+		return this.toString().equals(other.toString());
+	}
+	@Override
+	public final String toString() {
+		return JSON.toJson(this);
+	}
+}
